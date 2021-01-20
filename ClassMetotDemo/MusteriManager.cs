8b18c83729e55919a5bd37bff0bb7bdeba19afca
berkay akkoc <berkayakkocc6@gmail.com>
2021-01-20 11:25:27 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Tüm Musteri Bilgileri");
+            Console.WriteLine(musteri.Id);
+            Console.WriteLine(musteri.Ad);
+            Console.WriteLine(musteri.Soyad);
+            Console.WriteLine(musteri.Yas);
+
+        }
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 11111;
+            musteri1.Ad = "Berkaycan";
+            musteri1.Soyad = "Akkoç";
+            musteri1.Yas = 24;
+
+            
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 22222;
+            musteri2.Ad = "Altay";
+            musteri2.Soyad = "Akkoç";
+            musteri2.Yas = 19;
+
+            
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            Console.WriteLine("-------------");
+
+            musteriManager.Ekle(musteri1);
+            Console.WriteLine("-------------");
+            musteriManager.Sil(musteri2);
+
+        }
+    }
+}
